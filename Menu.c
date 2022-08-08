bb2e279d63a4992d45c694b35c26faf40bf745e8
RA341 <39427910+RA341@users.noreply.github.com>
2022-08-08 15:37:33 +0530

    added menu

---
+++ Menu.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include<string.h>
+
+char login[4][20]={"Manager","password","Clerk","password1"};
+int loginID = 1;
+int loop = 1;
+
+void main(){
+    while (loop == 1){
+
+        printf("Welcome please login \n");
+        char user[20];
+        char password[20];
+
+        printf("Enter username: ");
+        scanf("%s", user);
+
+        printf("Enter password: ");
+        scanf("%s", password);
+
+        //printf("%s \n",user);
+        //printf("%s \n",password);
+
+        //printf("%s \n",login[0]);
+        //printf("%s \n",login[1]);
+
+        int user_check = strcmp(login[0],user);
+        int pass_check = strcmp(login[1],password);
+
+        if(strcmp(login[0],user) == 0 && strcmp(login[1],password) == 0) {
+            printf("Welcome manager");
+            loginID = 1;
+        }
+
+        if(strcmp(login[2],user) == 0 && strcmp(login[3],password) == 0) {
+            printf("Welcome manager");
+            loginID = 0;
+        }
+
+        switch(loginID){
+            case 1:
+                    char exit ='';
+                    printf("exit y or n");
+                    scanf("&ch",exit);
+                    if (strcmp(exit,'y') == 0){
+                        break;
+                    }
+                    else{
+                        continue;
+                    }
+            case 0:
+                    char exit='';
+                    printf("exit y or n");
+                    scanf("&ch",exit);
+                    if (strcmp(exit,'y') == 0){
+                        break;
+                    }
+                    else{
+                        continue;
+                    }
+        }
+
+    }
+}
