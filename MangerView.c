52eed055fe6758721157d2716f6ef2cbdaf3c3bc
RA341 <razerishi1505@gmail.com>
2022-08-10 10:01:05 +0530

    added manager and clerk views

---
+++ MangerView.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+enum bool {false,true};
+
+
+void mainapp(){
+    int entry; 
+    char quit;
+    while (true)
+    {
+        printf("\nHello what would you like to do ? \n");
+        printf(" 1:fun1\n 2:fun2\n 3:fun3\n 4:fun4\n 5:fun5\n 6:fun6\n 7:exit\n");
+        printf("Enter option: ");
+        scanf("%d",&entry);
+
+        switch (entry){
+        case 7:
+            printf("\nAre you sure you want to exit the Program");
+            scanf(" %c",&quit); //leave this space for scanf
+
+            printf("%c \n",quit);
+
+            if (quit == 'y'){
+                exit(0);
+            }
+            else if(quit == 'n'){
+                continue;
+            }
+            else{
+                printf("\ninvalid input try again \n");
+            }
+        
+        default:
+            printf("Invalid choice try again");
+            continue;        
+        }
+    }
+    
+}
--- Menu.c
+++ Menu.c
@@ -1,47 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include "MangerView.c"
 
-enum bool {false,true};
 
 char login[4][20]={"Manager","password","Clerk","password1"};
 int loginID = 1;
 int loop = 1;
-
-void mainapp(){
-    int entry; 
-    char quit;
-    while (true)
-    {
-        printf("\nHello what would you like to do ? \n");
-        printf(" 1:fun1\n 2:fun2\n 3:fun3\n 4:fun4\n 5:fun5\n 6:fun6\n 7:exit\n");
-        printf("Enter option: ");
-        scanf("%d",&entry);
-
-        switch (entry){
-        case 7:
-            printf("\nAre you sure you want to exit the Program");
-            scanf(" %c",&quit); //leave this space for scanf
-
-            printf("%c \n",quit);
-
-            if (quit == 'y'){
-                exit(0);
-            }
-            else if(quit == 'n'){
-                continue;
-            }
-            else{
-                printf("\ninvalid input try again \n");
-            }
-        
-        default:
-            printf("Invalid choice try again");
-            continue;        
-        }
-    }
-    
-}
 
 void main(){
     while (loop == 1){
@@ -81,7 +46,7 @@ void main(){
                     mainapp();
                     
             case 0:
-                    continue;
+                    mainapp();
         }
 
     }
