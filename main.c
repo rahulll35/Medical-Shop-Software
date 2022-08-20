bea466ce4e6d7b4b542ca063cce1251239256832
ra341 <razerishi1505@gmail.com>
2022-08-20 17:56:33 +0530

    merged r5 requirment

--- main.c
+++ main.c
@@ -4,6 +4,12 @@
 
 enum bool {false,true};
 
+struct recipt{
+    char name[40];
+    int price;
+    int qty;
+};
+
 struct date{
       int day;
       int month;
@@ -18,7 +24,164 @@ struct details{
       struct date exp;
    };
 
-struct details item[50];
+typedef struct details Details;
+typedef struct recipt Recipt;
+Details item[50];
+Recipt bill[50];
+int no_of_items = 0;
+//linked list 
+struct Node {
+   char name[40];
+    int price;
+    int qty;
+    int code;
+   struct node *next;
+}; 
+
+typedef struct Node node;
+
+node *head = NULL;
+node* insertfirst(int code,char name[50],int price,int qty){
+    node* temp= (node*) malloc(sizeof(node));
+    
+    strcpy(temp -> name,name);
+    temp->price = price;
+    temp->qty=qty;
+    temp->code;
+
+    temp ->next=head;
+    temp = head;
+}
+
+void insertlast(int code,char name[50],int price,int qty){
+    node *temp = (node *) malloc(sizeof(node));
+
+    strcpy(temp -> name,name);
+    temp->price = price;
+    temp->qty=qty;
+    temp->code;
+
+    if (head == NULL)
+    {
+       head = temp;
+       temp  = NULL;
+       return; 
+    }
+
+    node *cur = head;
+
+    while (cur -> next != NULL)
+    {
+        cur = cur -> next;
+    }
+    cur -> next = temp;
+    temp = NULL;
+}
+
+void bill_delete(int code){
+
+    if (head == NULL)
+    {
+        printf("Empty list");
+        return;
+    }
+    
+    node* pre = NULL;
+    node* cur = head;
+    while (cur->code != code){   
+        pre = cur;
+        cur = cur->next;
+    }
+
+    if (pre == NULL){
+        printf("Element not found");
+        return;
+    }
+
+
+    node* temp = cur;
+    pre ->next = cur -> next;
+    temp ->next = NULL;
+    free(temp);
+}
+
+void display(){
+    if (head== NULL)
+    {
+        printf("Empty");
+        return;
+    }
+
+    node* temp = head;
+    while (temp->next != NULL)
+    {
+        printf("%d %s %d %d\n",temp->code,temp->name,temp->price,temp->qty);
+        temp = temp->next;
+    }
+
+    printf("%d %s %d %d\n",temp->code,temp->name,temp->price,temp->qty);
+}
+//end linkd list
+
+int get_item_index(int code,int lenght){
+    for (int x=0;x<lenght;x++){
+        if (code == item[x].code){
+            return x;
+        }
+    }
+}
+
+void gen_recipt(){
+    int choice=0,code,loop=1;
+    while (loop==1){
+        printf("1:Generate bill\n2:Delete from bill\n3:Display bill\n4:finalize bill\n5:Exit\n");
+        scanf("%d",&choice);
+
+        switch (choice)
+        {
+        case 1:
+            enter_details();
+            display();
+            break;
+        case 2:
+            display();
+            printf("Enter code of item to delete");
+            scanf("%d",&code);
+            bill_delete(code);
+            display();
+            break;
+        case 3:
+            display();
+            break;
+        case 4:
+            finalize_bill();
+            break;
+        case 5:
+            loop=0;
+            break;
+        default:
+            printf("\nInvalid choice try again\n");
+            break;
+        }
+    }
+}
+
+void enter_details(){
+    int code,flag=0,index=0;
+
+    printf("Enter med code");
+    scanf("%d",&code);
+
+    if (flag == 0){
+        index=get_item_index(code,no_of_items);
+        insertfirst(item[index].code,item[index].name,item[index].price,item[index].qty);
+    }else{
+        index = get_item_index(code,no_of_items);
+        insertlast(item[index].code,item[index].name,item[index].price,item[index].qty);
+    }
+}
+
+void finalize_bill(){
 
 
 char login[4][20]={"Manager","password","Clerk","password1"};
@@ -28,12 +191,11 @@ int loop = 1;
 
 //R1 - Inventory input
 void datainput(){
-
     int n,i;
     printf("Enter number of items:");
     scanf("%d",&n);
     fflush(stdin);
-    for(i=0;i<n;i++){
+    for(i=no_of_items;i< no_of_items+n;i++){
         fflush(stdin);
         printf("Item name:");
         scanf("%s",item[i].name);
@@ -53,11 +215,12 @@ void datainput(){
         printf("Expiry date(dd-mm-yyyy):");
         scanf("%d-%d-%d",&item[i].exp.day,&item[i].exp.month,&item[i].exp.year);
     }
+    no_of_items += n;
     printf(" ***** INVENTORY *****\n");
     printf("------------------------------------------------------------------\n");
     printf("S.N.| NAME | CODE | QUANTITY | PRICE | MFG.DATE | EXP.DATE |\n");
     printf("------------------------------------------------------------------\n");
-    for(i=0;i<n;i++)
+    for(i=0;i<no_of_items;i++)
         printf("%d %-15s %-d %-5d %-5d%d/%d/%d%d%d%d\n",i+1,item[i].name,item[i].code,item[i].qty,item[i].price,item[i].mfg.day,item[i].mfg.month,item[i].mfg.year,item[i].exp.day,item[i].exp.month,item[i].exp.year);
     printf("------------------------------------------------------------------\n");
     getchar();
